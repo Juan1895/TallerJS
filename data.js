e2e404a9aecfa1f86d6b612a2a58feaa6fc195df
Juan Esteban Castro Molano <107373334+juanescastro29@users.noreply.github.com>
2023-06-20 19:41:10 -0500

    Update data.js

--- data.js
+++ data.js
@@ -24,5 +24,17 @@ const students = {
       },
       phones: ["1234567890", "7752345", "3107637837"],
     },
+    201025666: {
+      name: "Juan Esteban",
+      lastName: "Castro Molano,
+      birthday: "2000-11-06",
+      gender: true,
+      address: {
+        street: 29,
+        number: 36,
+        city: "Duitama",
+      },
+      phones: ["320975121", "7752345", "3107637837"],
+    },
   },
 };
