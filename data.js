da8e8c885f9b9d06dc2db7ad59cd318d7147395c
StevenRincon24 <74875614+StevenRincon24@users.noreply.github.com>
2023-06-20 19:39:32 -0500

    Change Steven Rincon

--- data.js
+++ data.js
@@ -1,16 +1,28 @@
 const students = {
-    "Electiva-II" : {
-        "201025666" : {
-            "name" : "Jairo Armando",
-            "lastName" : "Riaño Herrera",
-            "birthday" : "1996-11-29",
-            "gender" : true,
-            "address" : {
-                "street" : 16,
-                "number" : 20,
-                "city" : "Sogamoso"
-            },
-            "phones":["3129083242","7752345","3107637837"]
-        }
-    }
-}
+  "Electiva-II": {
+    201025666: {
+      name: "Jairo Armando",
+      lastName: "Riaño Herrera",
+      birthday: "1996-11-29",
+      gender: true,
+      address: {
+        street: 16,
+        number: 20,
+        city: "Sogamoso",
+      },
+      phones: ["3129083242", "7752345", "3107637837"],
+    },
+    201025666: {
+      name: "Hovar Steven",
+      lastName: "Rincon Viancha",
+      birthday: "1998-11-24",
+      gender: true,
+      address: {
+        street: 16,
+        number: 20,
+        city: "Sogamoso",
+      },
+      phones: ["1234567890", "7752345", "3107637837"],
+    },
+  },
+};
